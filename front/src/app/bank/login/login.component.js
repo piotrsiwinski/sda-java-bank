daeabb3473986cd9f1bff48b9b9ce441b0b65988
Maciej Karczewski <m.karczewski1985@gmail.com>
2017-11-17 20:58:49 +0100

    Added register service

--- front/src/app/bank/login/login.component.js
+++ front/src/app/bank/login/login.component.js
@@ -7,7 +7,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require('@angular/core');
 var LoginComponent = (function () {
-    function LoginComponent() {
+    function LoginComponent(loginService, activatedRoute) {
+        this.loginService = loginService;
+        this.activatedRoute = activatedRoute;
     }
     LoginComponent.prototype.ngOnInit = function () {
     };
