15d824fac46ac645989d9d0d7634eb0232d15179
Maciej Karczewski <m.karczewski1985@gmail.com>
2017-11-17 19:40:43 +0100

    Routing module & PageNotFound component added

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule }              from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
