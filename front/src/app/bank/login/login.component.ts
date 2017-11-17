6e4d0201164ad9986f7bf6f77a7434fde2979d37
BorusiakTomasz <borusiak.tomasz@wp.pl>
2017-11-17 20:52:06 +0100

    Added login service and component

--- front/src/app/bank/login/login.component.ts
+++ front/src/app/bank/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from "./login.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'login',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
