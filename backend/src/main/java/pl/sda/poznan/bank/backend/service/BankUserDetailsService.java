58bdc18429dd17910b14e02a713e98ba73a6c695
andrzejnamerla <andrzej.namerla@gmail.com>
2017-11-20 18:48:03 +0100

    add BankUserDetailsService

---
+++ backend/src/main/java/pl/sda/poznan/bank/backend/service/BankUserDetailsService.java
@@ -0,0 +1,23 @@
+package pl.sda.poznan.bank.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.sda.poznan.bank.backend.model.User;
+import pl.sda.poznan.bank.backend.repository.UserRepository;
+
+public class BankUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.findnyLogin(login);
+        if(user == null){
+            throw new UsernameNotFoundException(login);
+        }
+        return new BankUserPricipal(user);
+    }
+}
