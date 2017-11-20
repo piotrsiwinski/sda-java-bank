b5c3bf0a14ebb9d8d5a13fbfd7f6f896a85e2231
andrzejnamerla <andrzej.namerla@gmail.com>
2017-11-20 20:15:05 +0100

    BankUserDetailsService, add authenticationProvider to WebSecurityConfig

--- backend/src/main/java/pl/sda/poznan/bank/backend/conffiguration/WebSecurityConfig.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/conffiguration/WebSecurityConfig.java
@@ -2,32 +2,42 @@ package pl.sda.poznan.bank.backend.conffiguration;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import pl.sda.poznan.bank.backend.service.BankUserDetailsService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+    @Autowired
     private RestLoginSuccessHandler restLoginSuccessHandler;
+
+    @Autowired
     private RestLoginFailureHandler restLoginFailureHandler;
-    private RestLogoutSuccessHandler restLogoutSuccessHandler;
+
+    @Autowired
+   private RestLogoutSuccessHandler restLogoutSuccessHandler;
+
+    @Autowired
     private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
     @Autowired
-    public WebSecurityConfig(RestLoginSuccessHandler restLoginSuccessHandler,
-                             RestLoginFailureHandler restLoginFailureHandler,
-                             RestLogoutSuccessHandler restLogoutSuccessHandler,
-                             RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
-        this.restLoginSuccessHandler = restLoginSuccessHandler;
-        this.restLoginFailureHandler = restLoginFailureHandler;
-        this.restLogoutSuccessHandler = restLogoutSuccessHandler;
-        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
-    }
+    private BankUserDetailsService userDetailsService;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -49,6 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
 
 
+
         http.csrf().disable();
         http.headers().frameOptions().disable();
     }
@@ -56,8 +67,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
+                .authenticationProvider(authenticationProvider());
 //                .userDetailsService()
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+//                .inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER");
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder(11);
     }
 }
