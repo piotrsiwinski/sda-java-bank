423f0448ddbecee9c21afa3957f456de5bc964cc
Maciej Karczewski <m.karczewski1985@gmail.com>
2017-11-20 19:58:10 +0100

    Added alert component

---
+++ front/src/app/_directives/alert-component.directive.spec.js
@@ -0,0 +1,9 @@
+"use strict";
+var alert_component_directive_1 = require('./alert-component.directive');
+describe('AlertComponentDirective', function () {
+    it('should create an instance', function () {
+        var directive = new alert_component_directive_1.AlertComponentDirective();
+        expect(directive).toBeTruthy();
+    });
+});
+//# sourceMappingURL=alert-component.directive.spec.js.map
---
+++ front/src/app/_directives/alert-component.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AlertComponentDirective } from './alert-component.directive';
+
+describe('AlertComponentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AlertComponentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
