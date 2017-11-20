3b5a701c010c800f1083a92a535d13bd1ef24e70
BorusiakTomasz <borusiak.tomasz@wp.pl>
2017-11-20 19:49:11 +0100

    Implement getAccess to login in UserService

--- front/src/app/_services/user.service.ts
+++ front/src/app/_services/user.service.ts
@@ -1,3 +1,23 @@
-/**
- * Created by RENT on 2017-11-20.
- */
+import {Injectable} from '@angular/core';
+
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAccess(email: string, password: string) {
+
+    let body = new URLSearchParams();
+    body.set('email', email);
+    body.set('password', password);
+
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+
+    return this.http.post('//yourUrl.com/login', body.toString(), options);
+  }
+}
