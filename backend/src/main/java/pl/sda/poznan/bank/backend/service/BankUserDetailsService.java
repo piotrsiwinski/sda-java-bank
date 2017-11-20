c8809c239cacae09f7636b78adb5b615d9ba0783
Szablewski <bartosz.szablewski123@wp.pl>
2017-11-20 18:58:32 +0100

    Add bankUserPrincipal

--- backend/src/main/java/pl/sda/poznan/bank/backend/service/BankUserDetailsService.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/service/BankUserDetailsService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.sda.poznan.bank.backend.model.BankUserPricipal;
 import pl.sda.poznan.bank.backend.model.User;
 import pl.sda.poznan.bank.backend.repository.UserRepository;
 
