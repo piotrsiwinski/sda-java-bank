6b715a6d14162d46e2361cd2c443a678eb9bbf5a
FilSos <filip.sosnowski88@gmail.com>
2017-11-20 19:12:54 +0100

    addtional functions

--- backend/src/main/java/pl/sda/poznan/bank/backend/service/BankAccountService.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/service/BankAccountService.java
@@ -1,35 +1,92 @@
+package bank.labs.service;
 
-package pl.sda.poznan.bank.backend.service;
-
-import org.springframework.context.annotation.Bean;
+import bank.labs.model.BankAccount;
+import bank.labs.model.Client;
+import bank.labs.model.History;
+import bank.labs.model.OperationType;
+import bank.labs.repository.HistoryRepository;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import pl.sda.poznan.bank.backend.model.AccountType;
-import pl.sda.poznan.bank.backend.model.BankAccount;
-import pl.sda.poznan.bank.backend.model.User;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import java.time.LocalDate;
 
 @Component
 public class BankAccountService {
 
+    private ClientService clientService;
+
+    private HistoryRepository historyRepository;
 
     private BankAccount bankAccount;
 
-    public Boolean payment(Long amount, User secondUser){
-        Long balance = bankAccount.getBalance();
-        if(amount!=null && amount > 0){
-            if(balance != null && balance > amount){
-                Long secondBalance = secondUser.getBankAccount().getBalance();
-                secondBalance +=amount;
-                balance -= amount;
-                return true;
-            }
 
+    @Autowired
+    public BankAccountService(ClientService clientService, HistoryRepository historyRepository, BankAccount bankAccount) {
+        this.clientService = clientService;
+        this.bankAccount = bankAccount;
+        this.historyRepository = historyRepository;
+    }
+
+
+    public Boolean payment(Double amount, Client client) {
+        Double balance = bankAccount.getBalance();
+
+        if (amount != null && amount > 0) {
+            balance += amount;
+            System.out.println(balance);
+            History history = new History(OperationType.CONTRIBUTION, LocalDate.now(),
+                    "Dokonano operacji wpłaty dnia: " + LocalDate.now() + " na kwotę " + amount);
+
+            historyRepository.save(history);
+            return true;
+        }
+        return false;
+    }
+
+
+    public Boolean payoff(Double amount) {
+        Double balance = bankAccount.getBalance();
+        if (amount != null && amount > 0) {
+            balance -= amount;
+            History history = new History(OperationType.CONTRIBUTION, LocalDate.now(),
+                    "Dokonano operacji wypłaty dnia: " + LocalDate.now() + " na kwotę " + amount);
+            historyRepository.save(history);
+            return true;
         }
         return false;
 
     }
+
+    //TODO session.getTransaction().isActive nie dziala, czemu???
+    public Boolean transfer(Double amount, Client secondClient) {
+        Session session = null;
+        Double balance = bankAccount.getBalance();
+        if (amount != null && amount > 0) {
+            if (balance != null && balance > amount) {
+                try {
+                    session.getTransaction().begin();
+                    Double secondClientBalance = secondClient.getBankAccount().getBalance();
+                    secondClientBalance += amount;
+                    balance -= amount;
+                    History history = new History(OperationType.CONTRIBUTION, LocalDate.now(),
+                            "Dokonano operacji przelewu dnia: " + LocalDate.now() + " na kwotę " + amount);
+                    historyRepository.save(history);
+                    session.getTransaction().commit();
+                    return true;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    if (session != null)
+                        session.getTransaction().rollback();
+                }
+
+            }
+        }
+
+        return false;
+
+    }
+
 
 }
 
