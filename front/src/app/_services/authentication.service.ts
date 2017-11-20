0ad612f61724c09719cc3bbdad893ff3a6095978
Maciej Karczewski <m.karczewski1985@gmail.com>
2017-11-20 19:57:25 +0100

    Authentication service fixed

--- front/src/app/_services/authentication.service.ts
+++ front/src/app/_services/authentication.service.ts
@@ -3,11 +3,12 @@ import {Http, Headers, Response, URLSearchParams} from '@angular/http';
 import 'rxjs/add/operator/map'
 import {HttpClient} from "@angular/common/http";
 import {HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
 
   }
 
@@ -21,6 +22,7 @@ export class AuthenticationService {
     return this.http.post('http://localhost;8080/api/authenticate', body.toString(), options)
       .subscribe(res => {
         console.log(res);
+        this.router.navigate(['/home']);
       }, err => {
         console.log("Error occured");
       });
