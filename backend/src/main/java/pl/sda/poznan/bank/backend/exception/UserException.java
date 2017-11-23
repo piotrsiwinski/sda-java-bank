bc15e3beb78378698b92a1c9bdeed9d09f852949
Szablewski <bartosz.szablewski123@wp.pl>
2017-11-23 20:15:03 +0100

    UserException -> TestUserLogin

--- backend/src/main/java/pl/sda/poznan/bank/backend/exception/UserException.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/exception/UserException.java
@@ -1,8 +1,5 @@
 package pl.sda.poznan.bank.backend.exception;
 
-import pl.sda.poznan.bank.backend.model.User;
-import pl.sda.poznan.bank.backend.service.UserService;
-
 public class UserException extends Exception {
 
     public UserException(String message) {
