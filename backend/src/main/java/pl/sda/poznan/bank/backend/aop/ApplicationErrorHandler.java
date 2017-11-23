a524fc354a82bce4a435c5cb838ada56b183f614
martazajac <xmartza1@gmail.com>
2017-11-23 19:48:04 +0100

    Added application error handler

---
+++ backend/src/main/java/pl/sda/poznan/bank/backend/aop/ApplicationErrorHandler.java
@@ -0,0 +1,44 @@
+package pl.sda.poznan.bank.backend.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.concurrent.TimeoutException;
+
+@ControllerAdvice
+@Slf4j
+public class ApplicationErrorHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(UsernameNotFoundException.class)
+    ResponseEntity<Object> handleUserNameNotFoundException(final UsernameNotFoundException ex) {
+        String bodyOfResponse = "404: The user was not found";
+        return new ResponseEntity<>(bodyOfResponse,
+                new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    ResponseEntity<Object> handleSQLException(HttpServletRequest request, Exception ex) {
+        String bodyOfResponse = "400: some exception thrown when executing the request.";
+        return new ResponseEntity<>(bodyOfResponse,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TimeoutException.class)
+    ResponseEntity<Object> handleTimeOutException(final TimeoutException ex) {
+        String bodyOfResponse = "408: The server timed out waiting for the request";
+        return new ResponseEntity<>(bodyOfResponse,
+                new HttpHeaders(), HttpStatus.REQUEST_TIMEOUT);
+
+    }
+
+
+}
+
+
