c9d839f031f795dd5cc95775be6a88f6407019b5
Szablewski <bartosz.szablewski123@wp.pl>
2017-11-23 18:02:05 +0100

    add BankAccountRepository && BankAccountService && BankAccountController.

---
+++ backend/src/main/java/pl/sda/poznan/bank/backend/repository/BankAccountRepository.java
@@ -0,0 +1,8 @@
+package pl.sda.poznan.bank.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.sda.poznan.bank.backend.model.BankAccount;
+
+public interface BankAccountRepository extends CrudRepository<BankAccount, Long> {
+
+}
---
+++ backend/src/main/java/pl/sda/poznan/bank/backend/service/BankAcconutService.java
@@ -0,0 +1,29 @@
+package pl.sda.poznan.bank.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.sda.poznan.bank.backend.model.BankAccount;
+import pl.sda.poznan.bank.backend.repository.BankAccountRepository;
+
+@Service
+public class BankAcconutService {
+
+    private BankAccountRepository bankAccountRepository;
+
+    @Autowired
+    public BankAcconutService(BankAccountRepository bankAccountRepository){
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    public void addAccount(BankAccount bankAccount){
+        bankAccountRepository.save(bankAccount);
+    }
+
+    public void deleteAccount(long id){
+        if ()
+       bankAccountRepository.delete(id);
+    }
+
+    public void upp
+
+}
