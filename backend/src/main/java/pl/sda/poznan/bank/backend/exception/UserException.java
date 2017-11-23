d00e4a27d3469a38e1918b87a19954f0b7dd2bf6
andrzejnamerla <andrzej.namerla@gmail.com>
2017-11-23 19:54:58 +0100

    UserException v1

--- backend/src/main/java/pl/sda/poznan/bank/backend/exception/UserException.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/exception/UserException.java
@@ -1,4 +1,17 @@
 package pl.sda.poznan.bank.backend.exception;
 
-public class UserException {
+import pl.sda.poznan.bank.backend.model.User;
+import pl.sda.poznan.bank.backend.service.UserService;
+
+public class UserException extends Exception {
+
+    private UserService userService;
+
+    public boolean UserEmailAlreadyRegistered(String UserEmail){
+        if (userService.findUserByEmail(UserEmail) == null){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
