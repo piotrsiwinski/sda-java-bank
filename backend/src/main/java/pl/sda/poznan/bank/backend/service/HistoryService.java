77d7ccd8261239f3557296343c959b782510763f
Szablewski <bartosz.szablewski123@wp.pl>
2017-11-27 18:15:29 +0100

    add HistoryService

--- backend/src/main/java/pl/sda/poznan/bank/backend/service/HistoryService.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/service/HistoryService.java
@@ -1,7 +1,26 @@
 package pl.sda.poznan.bank.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.sda.poznan.bank.backend.model.History;
+import pl.sda.poznan.bank.backend.model.User;
+import pl.sda.poznan.bank.backend.repository.HistoryRepository;
 
 @Service
 public class HistoryService {
+
+    private final HistoryRepository historyRepository;
+
+    @Autowired
+    public HistoryService(HistoryRepository historyRepository){
+        this.historyRepository = historyRepository;
+    }
+
+    public History findById(Long id){
+        return historyRepository.findById(id);
+    }
+
+    public History findByUser(User user){
+        return historyRepository.findByUser(user);
+    }
 }
