18db2a993c32782fe41a1c668266b9dbeb80e97d
andrzejnamerla <andrzej.namerla@gmail.com>
2017-11-27 18:25:46 +0100

    add HistroyController @GetMapping by id and user

--- backend/src/main/java/pl/sda/poznan/bank/backend/controller/api/v1/HistoryController.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/controller/api/v1/HistoryController.java
@@ -1,4 +1,36 @@
 package pl.sda.poznan.bank.backend.controller.api.v1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.poznan.bank.backend.repository.HistoryRepository;
+
+@RestController
+@RequestMapping("/api/v1/history")
 public class HistoryController {
+
+    private HistoryRepository historyRepository;
+
+    @Autowired
+    public HistoryController(HistoryRepository historyRepository){
+        this.historyRepository = historyRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView getHistory(@PathVariable("id") long id) {
+        ModelAndView modelAndView = new ModelAndView("historyId");
+        modelAndView.addObject("historyId", historyRepository.findById(id));
+        return modelAndView;
+    }
+
+    @GetMapping("/{user}")
+    public ModelAndView getUserHistory(@PathVariable("user") long user) {
+        ModelAndView modelAndView = new ModelAndView("userHistory");
+        modelAndView.addObject("userHistory", historyRepository.findById(user));
+        return modelAndView;
+    }
+
 }
