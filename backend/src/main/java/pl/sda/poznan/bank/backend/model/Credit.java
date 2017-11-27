b5b476c79e3e6ace95785a9c66ae4509b83cbbe1
FilSos <filip.sosnowski88@gmail.com>
2017-11-27 19:00:24 +0100

    added Credit, CreditService and CreditVM

---
+++ backend/src/main/java/pl/sda/poznan/bank/backend/model/Credit.java
@@ -0,0 +1,35 @@
+package pl.sda.poznan.bank.backend.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Credit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startCreditDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endCreditDate;
+    @ManyToOne
+    private User user;
+
+    private Double creditBalance;
+
+    private Double interest;
+
+    private Integer installment;
+
+}
+
