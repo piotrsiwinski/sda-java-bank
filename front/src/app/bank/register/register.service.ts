d0c533f44ae4925a16e5612120bb78a2c5f1c6d4
BorusiakTomasz <borusiak.tomasz@wp.pl>
2017-11-27 18:24:24 +0100

    register service

--- front/src/app/bank/register/register.service.ts
+++ front/src/app/bank/register/register.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class RegisterService {
+  email: string;
+  password: string;
 
   constructor(private http: HttpClient) { }
 
+  register(email: string, password: string) {
+    return this.http.post('/api/authenticate', JSON.stringify({ email: email, password: password }))
+      .map((response: Response) => {
+        console.log("register succes")
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+  }
 }
