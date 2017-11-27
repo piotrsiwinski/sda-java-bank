0a9be8f26baf574f114161ba9af668565c4ae78d
BorusiakTomasz <borusiak.tomasz@wp.pl>
2017-11-27 18:24:24 +0100

    register service

--- front/src/app/bank/register/register.component.ts
+++ front/src/app/bank/register/register.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {RegisterService} from "./register.service";
 
 @Component({
   selector: 'register',
@@ -6,11 +8,31 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  loading = false;
 
-  constructor() {
+  constructor(private router: Router,
+              private registerService: RegisterService) {
   }
 
   ngOnInit() {
+    this.registerService.logout();
   }
 
+  onClick() {
+    this.register();
+  }
+
+  register() {
+    this.loading = true;
+    this.registerService.register(this.registerService.email, this.registerService.password)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.log(error);
+          this.loading = false;
+        });
+  }
 }
