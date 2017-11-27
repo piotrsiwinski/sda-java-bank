b635df878602ac4d4fce36dffcd710e325ed01ce
FilSos <filip.sosnowski88@gmail.com>
2017-11-27 20:35:01 +0100

    working controller

--- backend/src/main/java/pl/sda/poznan/bank/backend/controller/api/v1/CreditController.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/controller/api/v1/CreditController.java
@@ -1,10 +1,13 @@
 package pl.sda.poznan.bank.backend.controller.api.v1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.sda.poznan.bank.backend.service.CreditServiceImpl;
 import pl.sda.poznan.bank.backend.web.viewmodel.CreditVM;
 import pl.sda.poznan.bank.backend.web.viewmodel.TransferVM;
 
@@ -13,16 +16,20 @@ import javax.validation.Valid;
 @Controller
 public class CreditController {
 
+    private CreditServiceImpl creditService;
 
-
-
+    @Autowired
+    public CreditController(CreditServiceImpl creditService) {
+        this.creditService = creditService;
+    }
+    //TODO sprawdz czy dobrze jest pobrany id
     @PostMapping(path = "/credit", consumes = "application/json")
-    public ResponseEntity getCredit(@RequestBody @Valid CreditVM viewModel, BindingResult bindingResult) {
+    public ResponseEntity getCredit(@RequestBody @Valid CreditVM viewModel, BindingResult bindingResult, @RequestParam("id") long id) {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
 
-        CreditVM.(viewModel);
+        creditService.getCredit(viewModel, id);
 
         return ResponseEntity.ok("Przelew sie udal");
     }
