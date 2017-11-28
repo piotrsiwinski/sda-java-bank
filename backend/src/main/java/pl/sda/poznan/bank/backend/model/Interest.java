227a9bb8c6fe2f345d6604d3d59c218800c7fd16
RENT <damiasz@op.pl>
2017-11-28 19:07:54 +0100

    InterestServiceImpl fix

--- backend/src/main/java/pl/sda/poznan/bank/backend/model/Interest.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/model/Interest.java
@@ -4,16 +4,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity
 @NoArgsConstructor
 @Getter
 @Setter
 public class Interest {
 
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private Double interestInPercent;
-    private LocalDate startDate;
-    private LocalDate endDate;
     private Investment investmentBalance;
     private AccountType accountType;
 }
