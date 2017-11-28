469a134e1c9ab45f93fa119ab6bb3684fc1bac58
Maciej Karczewski <m.karczewski1985@gmail.com>
2017-11-28 20:22:29 +0100

    Added login service

--- front/src/app/bank/login/login.service.ts
+++ front/src/app/bank/login/login.service.ts
@@ -4,7 +4,7 @@ import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 @Injectable()
 export class LoginService {
 
-  baseURL = 'http://localhost:8080/api/v1/';
+  baseURL = 'http://localhost:8080/api/v1/login';
 
   constructor(private http: HttpClient) {
 
@@ -15,8 +15,6 @@ export class LoginService {
     let params = new HttpParams()
       .set("login", formData.login)
       .set("password", formData.password)
-
-    let headers = new HttpHeaders().set("", "");
 
     return this.http.post(this.baseURL, {params: params}, {observe: 'response'}).subscribe(
       response => {
