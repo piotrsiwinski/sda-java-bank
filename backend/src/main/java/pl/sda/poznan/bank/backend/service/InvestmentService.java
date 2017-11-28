4e0431d8747caf282d6baef89aeb2ff47f0aa65f
Piotr S <siwinski.piotr@outlook.com>
2017-11-28 19:58:05 +0100

    Investment service refactor

--- backend/src/main/java/pl/sda/poznan/bank/backend/service/InvestmentService.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/service/InvestmentService.java
@@ -1,4 +1,17 @@
 package pl.sda.poznan.bank.backend.service;
 
+import pl.sda.poznan.bank.backend.model.Investment;
+
+import java.util.List;
+
 public interface InvestmentService {
+    Investment findInvestment(Long id);
+
+    List<Investment> findAllInvestments();
+
+    void saveInvestment(Investment investment);
+
+    void removeInvestment(Long id);
+
+    void updateInvestment(Long id, Investment newInvestment);
 }
--- backend/src/main/java/pl/sda/poznan/bank/backend/service/impl/InvestmentServiceImpl.java
+++ backend/src/main/java/pl/sda/poznan/bank/backend/service/impl/InvestmentServiceImpl.java
@@ -1,11 +1,51 @@
 package pl.sda.poznan.bank.backend.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.sda.poznan.bank.backend.model.Investment;
+import pl.sda.poznan.bank.backend.repository.InvestmentRepository;
 import pl.sda.poznan.bank.backend.service.InvestmentService;
+
+import java.util.List;
 
 
 @Service
 public class InvestmentServiceImpl implements InvestmentService {
+    private InvestmentRepository investmentRepository;
 
+    @Autowired
+    public InvestmentServiceImpl(InvestmentRepository investmentRepository) {
+        this.investmentRepository = investmentRepository;
+    }
+
+    public Investment findInvestment(Long id) {
+
+        return investmentRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Investment with this id doesn't exist"));
+    }
+
+    public List<Investment> findAllInvestments() {
+        return investmentRepository.findAll();
+    }
+
+    public void saveInvestment(Investment investment) {
+        investmentRepository.save(investment);
+    }
+
+    public void removeInvestment(Long id) {
+        investmentRepository.delete(id);
+    }
+
+    public void updateInvestment(Long id, Investment newInvestment) {
+        Investment oldInvestment = investmentRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Investment with this id doesn't exist"));
+        oldInvestment.setUser(newInvestment.getUser());
+        oldInvestment.setInterest(newInvestment.getInterest());
+        oldInvestment.setStartInvestmentDate(newInvestment.getStartInvestmentDate());
+        oldInvestment.setEndInvestmentDate(newInvestment.getEndInvestmentDate());
+        oldInvestment.setInvestmentBalance(newInvestment.getInvestmentBalance());
+    }
 
 }
