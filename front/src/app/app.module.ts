fd75b92f6bdbce76a7e9d28f2dd276062b1ffeef
Piotr S <siwinski.piotr@outlook.com>
2017-11-28 20:34:08 +0100

    Merge login component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { RouterOutlet } from '@angular/router';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {AppComponent} from './app.component';
 import {BankModule} from "./bank/bank.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatInputModule} from '@angular/material/input';
 import {AppRoutingModule} from "./app-routing.module";
 import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RegisterService} from "./bank/register/register.service";
 
 @NgModule({
   declarations: [
@@ -23,12 +23,13 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     AppRoutingModule,
+    HttpClientModule,
     MatIconModule,
     MatToolbarModule,
     MatButtonModule, MatIconModule, MatListModule, MatSidenavModule,
 
   ],
-  providers: [],
+  providers: [RegisterService],
   bootstrap: [AppComponent],
   schemas: [ NO_ERRORS_SCHEMA ]
 })
