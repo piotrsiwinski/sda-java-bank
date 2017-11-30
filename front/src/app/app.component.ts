2f4e051d59c5a520d1168957b94496650c59b410
Piotr S <siwinski.piotr@outlook.com>
2017-11-30 18:12:27 +0100

    Login and register refactor

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import {Component, NgModule, OnInit} from '@angular/core';
+import {Component, DoCheck, NgModule, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {AuthService} from "./bank/auth/auth.service";
+import {UserModel} from "./bank/model/UserModel";
+import {Router} from "@angular/router";
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule],
@@ -11,16 +13,37 @@ import {AuthService} from "./bank/auth/auth.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnChanges, DoCheck {
 
+  loggedUser: UserModel;
   title = 'app';
   isLoggedIn: Boolean = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   };
 
   ngOnInit(): void {
     this.isLoggedIn = this.authService.isAuthenticated();
+    this.loggedUser = {
+      firstName: 'Jan',
+      lastName: 'Kowalski',
+      email: 'test@wp.pl',
+    };
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  ngDoCheck() {
+    console.log("APP COMPONENT ngDoCheck");
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+
   }
 
 }
