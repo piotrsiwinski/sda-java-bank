58d02880bbe790069137d3849680bfb463610768
Piotr S <siwinski.piotr@outlook.com>
2017-12-04 19:54:17 +0100

    Added transfer service

--- front/src/app/transfer/transfer.service.ts
+++ front/src/app/transfer/transfer.service.ts
@@ -8,11 +8,11 @@ export class TransferService {
   }
 
   public createTransfer(data: any) {
-    let headers = new HttpHeaders()
-      .set('Content-type', 'application/json')
-      .set('Accept', 'application/json')
-      .set('withCredentials', 'true');
-    return this.http.post("http://localhost:8080/api/v1/deposit", data, {withCredentials: true, headers: headers});
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http
+      .post("http://localhost:8080/api/v1/deposit",
+        JSON.stringify(data),
+        {withCredentials: true, headers: headers});
   }
 
 }
